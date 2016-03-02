c47bde3c433c7899432041c521b8088cae021c82
Sarath <sarath.hacks@gmail.com>
2016-03-02 20:08:41 +0530

    Added operators / String formatting

--- basics/03-lists.py
+++ basics/03-lists.py
@@ -8,3 +8,25 @@
 numbers = [1,2,3,4,5,6]
 
 print(numbers[2])
+
+## Slicing List , slicing return a new String
+print(numbers[3:5])
+print(numbers[:])
+
+## Concatenate Lists
+list1 = [5,"Srath",4]
+list2 = [9,5.56,20]
+
+print(list1+list2)
+
+## Lists are mutable , we can change lists element value
+
+list1[1] = "x"
+
+print(list1)
+
+## Adding new items at the en using append methos
+
+list1.append("help")
+
+print(list1)
---
+++ basics/04-basic-operators.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+
+# Basic Operators
+
+
+## Addition
+
+print(20+5.5)
+
+## Subtraction
+
+print(60-6.9)
+
+## Division
+
+print(19/5)
+
+## Reminder
+
+print(19%5)
+
+## Power
+print(3**2)
+
+## String Concatenate
+print("Hello" + "World")
+
+## List Concatenate
+print([11,12,3]+[4,9,20])
